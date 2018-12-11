c852157e0f9ccf02128b0810cbf3095407f538c7
Ally <allison.avis@gmail.com>
2018-12-11 22:39:49 +0000

    character count function now turns text red, need to change position of the tweets within tweet container

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
+
   $(function() {
     $('.new-tweet textarea').on('keyup', function() {
         const characterCount = $(this).val().length;
         const charactersRemaining = (140 - characterCount);
         $('.new-tweet .counter').text(charactersRemaining);
         if (charactersRemaining < 0) {
-            $('.new-tweet .counter').addClass('red');
+            $('.new-tweet .counter').css('color', 'red')
         } else {
-            $('.new-tweet .counter').removeClass('red');
+            $('.new-tweet .counter').css('color', 'black');
         }
     });
   });
