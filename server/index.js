9de5d44de85ebb2afec5e5c661db21bf3701ef8e
Ally <allison.avis@gmail.com>
2018-12-11 01:51:31 +0000

    first commit - completed day 1 activities

--- server/index.js
+++ server/index.js
@@ -29,6 +29,11 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
 
+app.post('/tweets', (req, res) => {
+  const { tweet } = req.body.tweet;
+  res.redirect('/');
+});
+
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
