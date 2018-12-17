8a89f36ad76a5d19e725b901bfcf5542105050fa
Ally <allison.avis@gmail.com>
2018-12-17 05:13:40 +0000

    changed how time stamp works, wrote function in line instead of own file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,93 +1,131 @@
+function createTweetElement (tweet){
+    var $tweet = $("<article>").addClass("tweet");
+    var $header = $("<header>").appendTo($tweet);
+    var $avatar = $("<img>").addClass("avatar").attr("src", tweet.user.avatars.small).appendTo($header);
+    var $name = $("<h2>").text(tweet.user.name).appendTo($header);
+    var $handle = $("<p>").addClass("handle").text(tweet.user.handle).appendTo($header);
+    var $content = $("<p>").addClass("content").text(tweet.content.text).appendTo($tweet);
+    var $footer = $("<footer>").appendTo($tweet);
+    var $datePosted = $("<p>").addClass("date-posted").text(unixDate(tweet.created_at)).appendTo($footer);
+    var $socialIcons = $("<div>").addClass("icons").appendTo($footer);
+    var $like = $(`<i class="far fa-thumbs-up"></i>`).appendTo($socialIcons);
+    var $retweet = $(`<i class="fas fa-retweet"></i>`).appendTo($socialIcons);
+    var $flag = $(`<i class="fas fa-flag"></i>`).appendTo($socialIcons);
+    return $tweet;
+}
 
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
+function unixDate(digits){
+    const daysAgo = Math.floor((Date.now() - digits) / 86400000);
+    const hoursAgo = Math.floor((Date.now() - digits) / 3600000);
+    const minutesAgo = Math.floor((Date.now() - digits) / 60000);
+    if (daysAgo < 2 && hoursAgo < 2 && minutesAgo < 2){
+        return "Moments ago.";
+    } else if (daysAgo < 2 && hoursAgo < 2){
+        return minutesAgo + " minutes ago.";
+    } else if (daysAgo < 2 && hoursAgo >= 2){
+        return hoursAgo + " hours ago.";
+    } else {
+        return daysAgo + " days ago";
+    }
+}
 
 $(document).ready(function() {
 
-    const $error = $('div.error')
-    const $textArea = $('form textarea')
-
-    // function escape(text){
-    //     let escaped = $('<div/>').text(text).html()
-    //     return escaped;
-    // }
-
-    function createTweetElement(tweetDataObject){
-        const img = tweetDataObject.user.avatars.small;
-        const name = tweetDataObject.user.name;
-        const handle = tweetDataObject.user.handle;
-        const text = escape(tweetDataObject.content.text);
-        const timestamp = $.timeago(tweetDataObject.created_at);
-
-        let $tweet = $('<article>').addClass('tweet');
-        $tweet.append(`<header><img src="${img}"><h2>${name}</h2><span class='userID'>${handle}</span></header>`);
-        $tweet.append(`<p>${text}</p>`);
-        $tweet.append(`<footer>${timestamp}<i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-thumbs-up"></i></footer></article>`);
-        return $tweet;
-    }
-
-    function renderTweets(tweets) {
-        tweets.reverse().forEach(function(x, i){
-            const $tweet = createTweetElement(tweets[i]);
-            $('section.tweets-container').append($tweet);
+    function renderTweets(tweetDataArray){
+        // render tweets for an array of tweet data
+        tweetDataArray.forEach(function(item) {
+            // for each tweet in the array of tweet data, render it an append it to the
+            // .all-tweets section in index.html
+            var $tweet = createTweetElement(item);
+            // console.log("Tweet item: ", $tweet);
+            $(".all-tweets").prepend($tweet);
         });
     }
+    // function to handle the rendering of tweets into DOM structure (tree)
 
-    function loadTweets(){
-      $.get('/tweets', (data) => {
-        renderTweets(data);
-      });
+    function loadTweets (){
+        $.get("/tweets")
+        // this is getting the tweets from tweets.js through index.html
+            .done(tweets => {
+                // once the tweets have been received, render them
+                console.log("Got tweets! Rendering...");
+                renderTweets(tweets);
+            })
+            .fail(() => {
+                // if the tweets cannot be found, return an error
+                alert("Error");
+            });
     }
 
-    function clearTweets(){
-        $('section.tweets-container').empty();
-    }
-
-    function errorUser(message) {
-        if ($error.is(":visible")) {
-            $error.hide();
-        }
-
-        if (!message) {
-            $error.text("Nothing to tweet!").slideDown('fast');
-            return true;
-        } else if (message.length > 140) {
-            $error.text("Too much to tweet!").slideDown('fast');
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    $('form').on('submit', function(event) {
-      event.preventDefault();
-      const tweet = $textArea.val()
-      const $tweet = $('form').serialize();
-        if (errorUser(tweet)) {
-        } else {
-            $.post('/tweets/', $tweet, function(){
-                $textArea.val('');
-                $('.new-tweet .counter').text('140');
-                clearTweets();
-                loadTweets();
-                });
-        }
+    // Handles the toggling of the "Compose Tweet" field by clicking the
+    // ... "Compose" button. If the error field is visible, it will also
+    // slide up. When the compose button is clicked again to reveal the new-tweet
+    // text area, it will autoselect it.
+    $(function toggleCompose() {
+        var $navButton = $("#nav-bar .compose-button");
+        $navButton.on("click", function() {
+            $(".error-container").slideUp("slow");
+            $(".new-tweet-container").slideToggle("slow");
+            if ($(".new-tweet-container").is(":visible")){
+                $(".container .new-tweet-container .new-tweet .textarea").focus();
+            }
+        });
     });
 
-    $('#nav-bar .button').on('click', function(event){
-        if ($error.is(":visible")) {
-            $error.hide();
-        }
-        if ($('.new-tweet').is(":hidden")) {
-            window.scrollTo(0, 0);
-        }
-        $('.new-tweet').slideToggle('slow');
-        $textArea.select();
-    })
+    // Handles the close error button: toggles the error field when clicked.
+    $(function closeError() {
+        var $errorButton =  $(".container .new-tweet-container .new-tweet .error-container .error button");
+        $errorButton.on("click", function() {
+            $(".error-container").slideToggle();
+        });
+    });
 
-    loadTweets()
-});
+    $('form').on('submit', function(event) {
+        // when a submit event is called on "form", perform a function with argument "e" (for each event occurrence)
+        event.preventDefault();
+        // prevent the default, which is to reload the page
+        const formContent = $(this).serialize();
+        const formLength = $("textarea").val().length;
+        // serialize creates key:value pairs with the data entered ( in this case, key:value = text:<tweet message> )
+        // console.log('formContent', formLength);
+        function validateForm(){
+            if (formLength >= 140){
+                // Display the error field and add error text
+                $(".error-container").show();
+                $(".error-container .error p").text("Your tweet exceeds the maximum number of characters permitted.");
+                return false;
+            } else if (formLength == 0){
+                // Display the error field and add error text
+                $(".error-container").show();
+                $(".error-container .error p").text("Your tweet is empty.");
+                return false;
+            } else {
+                return true;
+            }
+        }
+        if (validateForm() === false){
+            event.stopPropagation();
+        } else {
+            // if successful, continue
+            $.ajax({
+                // sending the data to the server (asychronously)
+                method: 'POST',
+                url: '/tweets',
+                data: formContent,
+                success: function(data){
+                    $("form")[0].reset();
+                }
+            }).then(data => {
+                // after the data is sent, load the tweets, clear the error (if
+                // ... there is one), then clear the text area.
+                loadTweets();
+                $(".new-tweet .error-container").slideUp();
+                $(".new-tweet .textarea").text("");
+                $(".counter").text("140");
+            },
+            (err) => {
+                throw err;
+                })
+            }
+    });
+})
